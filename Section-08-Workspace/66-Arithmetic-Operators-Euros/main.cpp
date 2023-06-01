463194e02b9d6428eb2dc3f43620ddd8db59b40a
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-01 05:23:49 -0500

    Adding 66-Arithmetic-Operators-Euros files and directories

---
+++ Section-08-Workspace/66-Arithmetic-Operators-Euros/main.cpp
@@ -0,0 +1,31 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: arithmetic operators
+ *  Purpose: execute arithmetic operators
+ *  Date: 05/31/2023
+ * 
+ *  Notes:
+ * 
+ *          - 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    const double usd_per_euro {1.19};
+    double euros {0.0};
+    double dollars {0.0};
+    
+    cout << "Welcome to the EUR to USD converter " << endl;
+    cout << "Enter the value in Euros: ";
+    cin >> euros;
+    
+    dollars = euros * usd_per_euro;
+    cout << euros << " euros is equivalent to " << dollars << " dollars" << endl;
+    
+    cout << endl;
+    return 0;
+}
