e6235380cf8d6a868e4633596fa018b76c815f82
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-06-05 18:33:47 -0500

    Adding 74-Section-Challenge files and directories

---
+++ Section-08-Workspace/74-Section-Challenge/main.cpp
@@ -0,0 +1,49 @@
+/*
+ *  Author: Matt Johnson
+ *  Subject: section challenge
+ *  Purpose: 
+ *  Date: 06/05/2023
+ * 
+ *  Requirements:
+ * 
+ *          - Write a program that asks the user to enter the following:
+ *          
+ *          - Type an integer representing the number of cents
+ *              + You may assume that the number of cents entered is
+ *                  greater than or equal to zero
+ * 
+ *          - The program should display how to provide that change
+ *             to the user
+ * 
+ *          - In the United States:
+ *                  1 dollar is 100 cents
+ *                  1 quarter  is 25 cents
+ *                  1 dime is 10 cents
+ *                  1 nickel is 5 cents
+ *                  1 penny is 1 cent
+ * 
+ *          - Here is a sample run:
+ * 
+ *             Enter an amount in cents: 92
+ * 
+ *             You can provide this change as follows:
+ * 
+ *              dollars: 0
+ *              quarters: 3
+ *              dimes: 1
+ *              nickels: 1
+ *              pennies: 2
+ * 
+ *          - Hint: Try using the modulo operator
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    
+    cout << "This is the Section Challenge for Section 08\n" << endl;
+    
+    return 0;
+}
